93249ae0f5b9d652142dab1f8e2e2be80efa63b6
alexchtst-01 <acinatra@gmail.com>
2024-07-24 03:55:12 +0700

    msg: dummy.js for data response template and vercel.json to deploy in vercel (if necesary)

--- mobile/api/routes/userRoute.js
+++ mobile/api/routes/userRoute.js
@@ -1,7 +1,8 @@
-const {registerUser} = require('../controllers/userController');
+const {registerUser, getData} = require('../controllers/userController');
 const express = require('express');
 const router = express.Router();
 
 router.post('', registerUser);
+router.get('', getData);
 
 module.exports = router;
