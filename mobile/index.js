9cfdcafd93843c91b25ce10dec6ee20f7bf7b12d
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-07-24 15:34:17 +0700

    fix : minor fix

--- mobile/index.js
+++ mobile/index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_URI,
 ).
 then(() => {
     console.log("MongoDB Connected");
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT,"0.0.0.0", () => {
         console.log(`Server is running on port ${process.env.PORT}`);
     });
 }).catch((err) => {
